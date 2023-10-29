81a28d1afa02ac2258aee04debacbd75e1fff717
Abhiraj Bhattashali <abhirajbhattashali@gmail.com>
2023-10-29 19:34:24 +0530

    Initial Commit

---
+++ Streak/StreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakApp.swift
+//  Streak
+//
+//  Created by Abhiraj on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
